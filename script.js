05d5f4296c4f6fb8a0f8715989bd93d851e48633
S00what <zakolebal.exe@gmail.com>
2026-08-18 20:27:59 +0500

    first commit added js with answers and coordinates x y z

---
+++ script.js
@@ -0,0 +1,21 @@
+// a JS hw
+var name = prompt("Print your name please");
+console.log("Your name is " + name);
+var age = Number(prompt("Print your age please"));
+console.log("Your age is " + age);
+var sum1 = Number(prompt("6 + 3 = ?"));
+var sum2 = Number(prompt("9 * 12 = ?"));
+var sum3 = Number(prompt("26 / 4 = ?"));
+var sum4 = Number(prompt("60 - 42 = ?"));
+var sum5 = Number(prompt("50 % 12 = ?"));
+console.log("6 + 3 = " + (6+3) + " Your answer " + sum1);
+console.log("9 * 12 = " + (9*12) + " Your answer " + sum2);
+console.log("24 / 4  = " + (24/4) + " Your answer " + sum3);
+console.log("60 - 42 = " + (60-42) + " Your answer " + sum4);
+console.log("50 % 12 = " + (50%12) + " Your answer " + sum5);
+// b JS hw
+var X = Number(prompt("Please print your coordinates X"))
+var Y = Number(prompt("Please print your coordinates Y"))
+var Z = Number(prompt("Please print your coordinates Z"))
+
+alert("The arithmetic mean of your coordinates " + (X+Y+Z) / 3)
