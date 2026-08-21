d7033f09bebc358dc221d81fdfc7bb4afd3085c7
S00what <zakolebal.exe@gmail.com>
2026-08-21 13:54:24 +0500

    First task completed with age

--- script.js
+++ script.js
@@ -13,38 +13,4 @@ if (age < 18 && !isNaN(age)) {
 } else {
   alert("You alive?!");
 }
-// second task
-let time = +prompt("Enter the time");
-if (isNaN(time) || time < 0 || time > 23) {
-  alert("Isnt time");
-} else if (time < 6) {
-  alert(`${time} a.m.`);
-} else if (time <= 10) {
-  alert(`${time} am`);
-} else if (time <= 17) {
-  alert(`${time} pm`);
-} else {
-  alert(`${time} pm`);
-}
-//third task
-let num1 = +prompt("Enter first number");
-let num2 = +prompt("Enter second number");
-let num3 = +prompt("Enter third number");
 
-if (num1 > num2 && num1 < num3) {
-  alert(`Среднее число между ${num2} и ${num3}. Это ${num1}`);
-} else if (num1 > num3 && num1 < num2) {
-  alert(`Среднее число между ${num2} и ${num3}. Это ${num1}`);
-} else if (num2 > num3 && num2 < num1) {
-  alert(`Среднее число между ${num1} и ${num3}. Это ${num2}`);
-} else if (num2 > num1 && num2 < num3) {
-  alert(`Среднее число между ${num1} и ${num3}. Это ${num2}`);
-} else if (num3 > num2 && num3 < num1) {
-  alert(`Среднее число между ${num2} и ${num1}. Это ${num3}`);
-} else if (num3 > num1 && num3 < num2) {
-  alert(`Среднее число между ${num2} и ${num1}. Это ${num3}`);
-} else if (isNaN(num1) || isNaN(num2) || isNaN(num3)) {
-  alert("Все три числа должны быть числом");
-} else {
-  alert("Something went wrong");
-}
