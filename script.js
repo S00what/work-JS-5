8455806a84aaac322005142ffb4de5dca0afd412
S00what <zakolebal.exe@gmail.com>
2026-08-25 20:24:26 +0500

    All tasks completed

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
-// first task
-let bananaNum = Number(prompt('Enter numbers bananas'))
-if ((bananaNum == 1) && !isNaN(bananaNum) && bananaNum) {
-  alert('1 banana')
+//first task
+let bananaNum = Number(prompt('Enter number'))
+
+while (bananaNum -= 1) {
+  if (bananaNum === 1) {
+    console.log('1 banana'); 
+  }
+  else{
+  console.log (bananaNum + " bananas")
+  }
 }
-else if (bananaNum > 0 && !isNaN(bananaNum)) {
-  alert(`${bananaNum} bananas`)
+//second task 
+var num = Number(prompt('Enter number Ill count all its even numbers and output them to the console'))
+var sumEvenNum = 0
+
+for (var i = 1; i <= num; i++) {
+  if(i % 2 === 0) {
+    sumEvenNum += i
+  }
 }
-else {
-  alert('Something went wrong')
-}
+console.log(sumEvenNum);
+//third task
+  let numForDegree = Number(prompt('Enter number for degree'))
+  let degree = Number(prompt('Enter degree for number'))
+
+  alert(numForDegree ** degree)
