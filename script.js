d7f42e88d89d7201298e89485b78c27c9c5c7569
S00what <zakolebal.exe@gmail.com>
2026-08-28 16:18:37 +0500

    First task completed

--- script.js
+++ script.js
@@ -1,26 +1,10 @@
 //first task
-let bananaNum = Number(prompt('Enter number'))
-
-while (bananaNum -= 1) {
-  if (bananaNum === 1) {
-    console.log('1 banana'); 
-  }
-  else{
-  console.log (bananaNum + " bananas")
-  }
-}
-//second task 
-var num = Number(prompt('Enter number Ill count all its even numbers and output them to the console'))
-var sumEvenNum = 0
-
-for (var i = 1; i <= num; i++) {
-  if(i % 2 === 0) {
-    sumEvenNum += i
-  }
-}
-console.log(sumEvenNum);
-//third task
-  let numForDegree = Number(prompt('Enter number for degree'))
-  let degree = Number(prompt('Enter degree for number'))
-
-  alert(numForDegree ** degree)
+let name;
+let todayYear;
+let birthYear;
+do {
+  name = String(prompt("Enter youre name"));
+  todayYear = Number(prompt("Enter today`s year"));
+  birthYear = Number(prompt('Enter birth of year'))
+} while (typeof name !== "string" || !todayYear || !birthYear); // Спросил у ии как сократить IsNaN null 0 ответил так "!"
+output.textContent = `Your name is ${name} your age ${todayYear - birthYear}`
