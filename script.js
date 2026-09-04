2c3b2a4ade5afd5671284d9e55345e2b59ef4971
S00what <zakolebal.exe@gmail.com>
2026-09-04 19:51:26 +0500

    Refactoring code

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-//first task
- let name;
- let todayYear;
- let birthYear;
- do {
-   name = String(prompt("Enter youre name"));
-   todayYear = Number(prompt("Enter today`s year"));
-   birthYear = Number(prompt("Enter birth of year"));
- } while (typeof name !== "string" || !todayYear || !birthYear); // Спросил у ии как сократить IsNaN null 0 ответил так "!"
- output.textContent = `Your name is ${name} your age ${todayYear - birthYear}`;
+// //first task
+let name, todayYear, birthYear;
+for (;typeof name !== "string" || !todayYear || !birthYear;) {
+  name = String(prompt("Enter youre name"));
+  todayYear = Number(prompt("Enter today`s year"));
+  birthYear = Number(prompt("Enter birth of year"));
+} // Спросил у ии как сократить IsNaN null 0 ответил так "!"
+output.textContent = `Your name is ${name} your age ${todayYear - birthYear}`;
 //second task
-let userAnswer, realAnswer, userMin, userMax;
+let userAnswer, sysAnswer, userMin, userMax, a, b;
 
 function numSymbol(min = 1, max = 4) {
   return Math.floor(Math.random() * (max + 1 - min) + min);
@@ -17,48 +15,43 @@ function numSymbol(min = 1, max = 4) {
 function randomNum(userMin, userMax) {
   return Math.floor(Math.random() * (userMax + 1 - userMin) + userMin);
 }
-
+userMin = Number(prompt("Enter min value"));
+userMax = Number(prompt("Enter max value"));
 do {
-  userMax = Number(prompt("Enter max number for the equation"));
-  userMin = Number(prompt("Enter min number for the equation"));
-} while (!userMax || !userMin);
-
-do {
-  let symbol = numSymbol();
-  let a = randomNum(userMin, userMax);
-  let b = randomNum(userMin, userMax);
-
-  if (symbol === 1) {
+  a = randomNum(userMax, userMin);
+  b = randomNum(userMax, userMin);
+  equa = numSymbol();
+  if (equa == 1) {
+    sysAnswer = a + b;
     userAnswer = Number(prompt(`${a} + ${b} = ?`));
-    realAnswer = a + b;
-    if (realAnswer == userAnswer) {
-      alert(`a + b = ${realAnswer}. You right`);
+    if (sysAnswer == userAnswer && !userAnswer) {
+      alert(`${a} + ${b} = ${sysAnswer} your right`);
     } else {
-      alert(`a + b = ${realAnswer}. You isnt right youre answer ${userAnswer}`);
+      alert(`${a} + ${b} = ${sysAnswer} your isnt right (${userAnswer})`);
     }
-  } else if (symbol === 2) {
-    userAnswer = Number(prompt(`${a} * ${b} = ?`));
-    realAnswer = a * b;
-    if (realAnswer == userAnswer) {
-      alert(`a + b = ${realAnswer}. You right`);
-    } else {
-      alert(`a + b = ${realAnswer}. You isnt right youre answer ${userAnswer}`);
-    }
-  } else if (symbol === 3) {
-    userAnswer = Number(prompt(`${a} / ${b} = ?`));
-    realAnswer = a / b;
-    if (realAnswer == userAnswer) {
-      alert(`a + b = ${realAnswer}. You right`);
-    } else {
-      alert(`a + b = ${realAnswer}. You isnt right youre answer ${userAnswer}`);
-    }
-  } else if (symbol === 4) {
+  } else if (equa == 2) {
+    sysAnswer = a - b;
     userAnswer = Number(prompt(`${a} - ${b} = ?`));
-    realAnswer = a - b;
-    if (realAnswer == userAnswer) {
-      alert(`a + b = ${realAnswer}. You right`);
+    if (sysAnswer == userAnswer && !userAnswer) {
+      alert(`${a} - ${b} = ${sysAnswer} your right`);
     } else {
-      alert(`a + b = ${realAnswer}. You isnt right. Youre answer ${userAnswer}`);
+      alert(`${a} - ${b} = ${sysAnswer} your isnt right (${userAnswer})`);
+    }
+  } else if (equa == 3) {
+    sysAnswer = a / b;
+    userAnswer = Number(prompt(`${a} / ${b} = ?`));
+    if (sysAnswer == userAnswer && !userAnswer) {
+      alert(`${a} / ${b} = ${sysAnswer} your right`);
+    } else {
+      alert(`${a} / ${b} = ${sysAnswer} your isnt right (${userAnswer})`);
+    }
+  } else if (equa == 4) {
+    sysAnswer = a * b;
+    userAnswer = Number(prompt(`${a} * ${b} = ?`));
+    if (sysAnswer == userAnswer && !userAnswer) {
+      alert(`${a} * ${b} = ${sysAnswer} your right`);
+    } else {
+      alert(`${a} * ${b} = ${sysAnswer} your isnt right (${userAnswer})`);
     }
   }
-} while (!userAnswer);
+} while (!userAnswer); //ПРишлось использовать do while из за построчного читаемого кода ведь userAnswer должен быть в цикле а если использовать for то он читается первым и userAnswer является undefined(не знаю как это исправить)
