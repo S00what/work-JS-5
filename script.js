a7740d5b87fd41c2614310aa4850c7c51acd461d
S00what <zakolebal.exe@gmail.com>
2026-09-08 18:53:19 +0500

    Reworking code to suit wishes

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
-let users = {};
-let i = 0;
-do {
+let users = {}, newI = 0;
+for (let i = 0; i != 10; ) {
   users[`user${i}`] = {};
   users[`user${i}`].name = String(prompt(`Enter your name (${i})`));
   let age = Number(prompt(`Enter your age (${i})`));
-  while (isNaN(age)) {
-    age = Number(prompt(`Введите число (${i})`));
-  }
   users[`user${i}`].age = age;
-  console.log(
-    `Name is ${users[`user${i}`].name}, age is ${users[`user${i}`].age}, number user ${i}`,
-  );
   i++;
-} while (i != 10);
-console.log(users);
-//Прошу простить использование do while скоро исправлюсь и придумаю как его не использовать
+}
+for (const key in users) {
+  console.log(
+    `Name is ${users[`user${newI}`].name}, age is ${users[`user${newI}`].age}, number user ${newI}`,
+  );
+  newI++;
+}
